462bc5c09655273811206a57e33aa5fb321e18b2
the0xjerry <jerryjoekaizer@gmail.com>
2021-11-21 23:26:54 +0100

    added missing files

--- src/Contracts/Thether.sol
+++ src/Contracts/Thether.sol
@@ -3,7 +3,7 @@
 pragma solidity >=0.5.0 < 0.9.0;
 
 contract Thether {
-    string public name = 'Thether';
+     public name = 'Thether';
    string public symbol = 'USDT';
     uint public totalSupply = 1000000000000000000; //1 Million tokens
     uint public decimal = 18;
